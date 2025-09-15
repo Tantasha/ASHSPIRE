6859c5452f7d07d302dc461ee40315de34b5eb9c
Tasha Kim <146703939+Tantasha@users.noreply.github.com>
2025-09-15 15:15:25 +1200

    new commit

---
+++ MainMenu/Main Menu/Assets/Scripts/confirmationWindow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class confirmationWindow : MonoBehaviour
+{
+    public Button yesButton;
+    public Button noButton;
+    public TextMeshProUGUI messageText;
+
+    
+}
