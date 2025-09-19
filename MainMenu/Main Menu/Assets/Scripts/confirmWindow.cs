f01179720a3192ddbe929480aac179de696e8971
Tasha Kim <146703939+Tantasha@users.noreply.github.com>
2025-09-20 02:18:42 +1200

    commit

---
+++ MainMenu/Main Menu/Assets/Scripts/confirmWindow.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class confirmWindow : MonoBehaviour
+{
+    public Button yesBtn;
+    public Button noBtn;
+    public TextMeshProUGUI message;
+}
