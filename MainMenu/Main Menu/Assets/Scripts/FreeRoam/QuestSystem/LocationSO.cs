9a3587f8c74a3edb2921631f084c92c967b8a2b7
johnxrusty <johnxcatalan@gmail.com>
2025-09-26 10:35:06 +1200

    QuestSystem
    Not nearly done

---
+++ MainMenu/Main Menu/Assets/Scripts/FreeRoam/QuestSystem/LocationSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LocationSO")]
+public class LocationSO : ScriptableObject
+{
+    public string LocationID;
+    public string displayName;
+
+}
