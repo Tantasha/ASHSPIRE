525a4fca9096ba477063903f78f0091f336033be
Tasha Kim <146703939+Tantasha@users.noreply.github.com>
2025-09-26 14:13:02 +1200

    name changes

--- Main Menu/Assets/Scripts/ItemCollection.cs
+++ Main Menu/Assets/Scripts/ItemCollection.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 
-public class GemCollection : MonoBehaviour
+public class ItemCollection : MonoBehaviour
 {
     public DragonGrowthControl growthControl;
-    public float progressPerGem = 0.2f;
+    public float progressPerGem = 0.5f;
+    public float progressPerFood = 0.5f;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Triggered with: " + collision.name);
-        if (collision.CompareTag("Gem"))
+        if (collision.CompareTag("Gem") || collision.CompareTag("Food"))
         {
             growthControl.AddProgress(progressPerGem);
             Destroy(collision.gameObject);
