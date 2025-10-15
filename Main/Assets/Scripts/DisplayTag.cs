52efb1fbae529f0bc3104f936922bdc190cd64f8
Kokwahyee <114566117+Kokwahyee@users.noreply.github.com>
2025-10-16 00:03:36 +1300

    implemented and updated the code

--- Main/Assets/Scripts/DisplayTag.cs
+++ Main/Assets/Scripts/DisplayTag.cs
@@ -2,6 +2,7 @@
 
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 namespace ReadInputNameTag
 {
@@ -9,14 +10,46 @@ namespace ReadInputNameTag
     {
 
         [SerializeField] private TextMeshProUGUI textElement;
-        private string NameTag;
+        private static List<DisplayTag> allDisplays = new List<DisplayTag>();
 
-        private void Update()
+
+        private void Awake()
         {
-            if (ReadInputNameTag.input1 != NameTag)
+            // Register this display
+            allDisplays.Add(this);
+        }
+
+
+        private void OnDestroy()
+        {
+            // Unregister when destroyed
+            allDisplays.Remove(this);
+        }
+
+
+
+        private void Start()
+        {
+            // Load and display saved name immediately
+            if (PlayerPrefs.HasKey("SavedNameTag"))
             {
-                NameTag = ReadInputNameTag.input1;
-                textElement.text = NameTag;
+                string savedName = PlayerPrefs.GetString("SavedNameTag");
+                textElement.text = savedName;
+            }
+            else
+            {
+                textElement.text = "";
+            }
+        }
+
+        // Called when any DisplayTag needs to refresh
+        public static void UpdateAllDisplays()
+        {
+            string latestName = PlayerPrefs.GetString("SavedNameTag", "");
+            foreach (var display in allDisplays)
+            {
+                if (display.textElement != null)
+                    display.textElement.text = latestName;
             }
         }
 
