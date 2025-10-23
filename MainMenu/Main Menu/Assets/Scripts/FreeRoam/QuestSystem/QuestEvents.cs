a31f80ca231f63d4377f5067087c797c8719a06c
johnxrusty <johnxcatalan@gmail.com>
2025-10-23 16:52:10 +1300

    Trial

--- MainMenu/Main Menu/Assets/Scripts/FreeRoam/QuestSystem/QuestEvents.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-public static class QuestEvents
-{
-    public static Action<QuestSO> OnQuestOfferRequested;
-    public static Action<QuestSO> OnQuestTurnInRequested;
-    public static Func<QuestSO, bool> IsQuestComplete;
-}
