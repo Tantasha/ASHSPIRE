3735395977b1899fde56714668d714fb9dc862f7
Tasha Kim <146703939+Tantasha@users.noreply.github.com>
2025-10-31 12:48:48 +1300

    Delete Scripts directory

--- Scripts/SceneTracker.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public static class SceneTracker
-{
-    public static string CurrentSceneName;
-}
--- Scripts/WorldTimeConstant.cs
+++
@@ -1,9 +0,0 @@
-
-namespace WorldTime
-{
-    public static class WorldTimeConstant 
-    {
-        public const int MinutesInDay = 1440;//totak number of minutes in a full day
-    }
-}
-
